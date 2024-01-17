fac4809d98623879cc6f6a7bb802055031c64f43
Fortichoun <a.surin@hemea.com>
2024-01-17 18:52:32 +0100

    initialisation

---
+++ init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt update
+
+git config --global user.email "a.surin@hemea.com"
+git config --global user.name "Fortichoun"
+
+# sudo snap install intellij-idea-ultimate --classic
+
